602a8a03f9cc758b588bce625d4b3c89be9131f3
Tobias Grosser <grosser@fim.uni-passau.de>
2010-12-15 23:50:09 -0500

    python bindings: Add CodeCompletionResults

---
+++ bindings/python/tests/cindex/test_code_completion.py
@@ -0,0 +1,12 @@
+from clang.cindex import *
+
+def code_complete_source(source, line, column):
+    index = Index.create()
+    files = [('INPUT.c', source)]
+    tu = index.parse('INPUT.c', unsaved_files = files)
+    return tu.codeComplete('INPUT.c', line, column, unsaved_files = files)
+
+def test_completion_results():
+    cr = code_complete_source(""" """, 1, 1)
+    assert len(cr.results) > 0 and len(cr.results) < 1000
+
